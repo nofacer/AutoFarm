33bbecc0bc5dde2c33a5810b11e7822b75d606d6
Dustni <nofacer@outlook.com>
2017-04-16 19:46:40 +0800

    fix

--- receive.py
+++ receive.py
@@ -1,12 +1,16 @@
 import serial
 import random
+import time
 #ser=serial.Serial('Com3',9600)
 ser=serial.Serial('/dev/ttyUSB0',9600)
 
 def get_temper():
+    nowtime = time.asctime(time.localtime(time.time()))
     data=ser.readline()
-    return data.decode()[:-2]
+
     # return str(random.uniform(0, 100))
     file=open("record.csv",'wb')
-    file.write(data.decode()[:-2])
+    file.write(nowtime+'   '+data.decode()[:-2]+'\n')
     file.close()
+
+    return data.decode()[:-2]
